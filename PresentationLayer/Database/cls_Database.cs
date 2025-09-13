ef51a5582733eddcfb5c5f24fcedc72ed12677a4
mostafamohm2002ead@gmail.com <mostafamohm2002ead@gmail.com>
2025-09-13 16:21:03 +0300

    Add Category Model, Controller & index view

---
+++ PresentationLayer/Database/cls_Database.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PresentationLayer.Models;
+
+namespace PresentationLayer.Database
+{
+    public class cls_Database : DbContext
+    {
+        public cls_Database(DbContextOptions<cls_Database> options) : base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        }
+}
--- PresentationLayer/Program.cs
+++ PresentationLayer/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using PresentationLayer.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<cls_Database>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
